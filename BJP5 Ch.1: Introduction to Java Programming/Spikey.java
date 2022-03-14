e44ae33cbc6042ac2675638695160d224264390d
cchung100 <101427703+cchung100@users.noreply.github.com>
2022-03-14 18:07:10 -0400

    Create Spikey.java

---
+++ BJP5 Ch.1: Introduction to Java Programming/Spikey.java
@@ -0,0 +1,27 @@
+/*
+~ File Name: Spikey.java
+~ File Purpose: Spikey.java is a solution for BJP5 Exercise 1.2: Spikey. The prompt is below.
+Write a complete Java program in a class named Spikey that prints the following output:
+  \/
+ \\//
+\\\///
+///\\\
+ //\\
+  /\
+~ Break the problem into objectives:
+OBJ1. Create a class named Spikey.
+OBJ2. Create a main method.
+OBJ3. Print the message in the main method.
+*/
+
+public class Spikey {   // OBJ1
+    public static void main(String[] args) {  // OBJ2
+      // OBJ3 is the 6 println statements below
+        System.out.println("  \\/");   // Be mindful of spacing and backslashes
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///"); 
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\");
+    }                                                                                                             
+}
