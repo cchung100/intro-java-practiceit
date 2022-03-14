d270fc1a37a1004f139f1f05915dd545dd3575ad
cchung100 <101427703+cchung100@users.noreply.github.com>
2022-03-14 18:06:29 -0400

    Create Stewie.java

---
+++ BJP5 Ch.1: Introduction to Java Programming/Stewie.java
@@ -0,0 +1,21 @@
+/*
+~ File Name: Stewie.java
+~ File Purpose: Stewie.java is a solution for BJP5 Exercise 1.1: Stewie. The prompt is below.
+Write a complete Java program in a class named Stewie that prints the following output:
+//////////////////////
+|| Victory is mine! ||
+\\\\\\\\\\\\\\\\\\\\\\
+~ Break the problem into objectives:
+OBJ1. Create a public class named Stewie.
+OBJ2. Create a main method.
+OBJ3. Print the message in the main method.
+*/
+
+public class Stewie {   // OBJ1
+    public static void main(String[] args) {  // OBJ2
+      // OBJ3 is the 3 println statements below
+        System.out.println("//////////////////////");
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");  // Use \\ not \ because \\ is an escape sequence for displaying a backslash.
+    }                                                                                                             
+}
