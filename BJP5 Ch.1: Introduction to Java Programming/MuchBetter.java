32f73e0947e2937143db4620e43b1a232dfe6cc5
cchung100 <101427703+cchung100@users.noreply.github.com>
2022-03-14 19:29:09 -0400

    Create MuchBetter.java

---
+++ BJP5 Ch.1: Introduction to Java Programming/MuchBetter.java
@@ -0,0 +1,19 @@
+/*
+~ File Name: MuchBetter.java
+~ File Purpose: MuchBetter.java is a solution for BJP5 Exercise 1.5: MuchBetter. The prompt is below.
+Write a complete Java program in a class named MuchBetter that prints the following output:
+A "quoted" String is
+'much' better if you learn
+the rules of "escape sequences."
+Also, "" represents an empty String.
+Don't forget: use \" instead of " !
+'' is not the same as "
+*/
+
+public class MuchBetter {   // OBJ1
+    public static void main(String[] args) {  // OBJ2
+        // OBJ3 is the println statements below
+        System.out.println("A \"quoted\" String is\n'much' better if you learn\nthe rules of \"escape sequences.\"\nAlso, \"\" represents an empty String.\nDon't forget: use \\\" instead of \" !\n'' is not the same as \"");
+       // To show more examples of escape sequences, I wrote a single statement with multiple \n (new line) instead of a series of printlns.
+    }
+}
