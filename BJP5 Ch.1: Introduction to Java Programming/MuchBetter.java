21591a518c2731fda0caa6d1cddfc2dfb6713b01
cchung100 <101427703+cchung100@users.noreply.github.com>
2022-03-15 00:11:53 -0400

    Update MuchBetter.java

--- BJP5 Ch.1: Introduction to Java Programming/MuchBetter.java
+++ BJP5 Ch.1: Introduction to Java Programming/MuchBetter.java
@@ -8,11 +8,15 @@ the rules of "escape sequences."
 Also, "" represents an empty String.
 Don't forget: use \" instead of " !
 '' is not the same as "
+~ Break the problem into objectives:
+OBJ1. Create a class named MuchBetter.
+OBJ2. Create a main method.
+OBJ3. Print the message in the main method.
 */
 
 public class MuchBetter {   // OBJ1
     public static void main(String[] args) {  // OBJ2
-        // OBJ3 is the println statements below
+        // OBJ3 is the println statement below
         System.out.println("A \"quoted\" String is\n'much' better if you learn\nthe rules of \"escape sequences.\"\nAlso, \"\" represents an empty String.\nDon't forget: use \\\" instead of \" !\n'' is not the same as \"");
        // To show more examples of escape sequences, I wrote a single statement with multiple \n (new line) instead of a series of printlns.
     }
